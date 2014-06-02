a10488a16bd6dada9b7672076fa30974df59216a
Denis Pismenny <denis.pismenny@gmail.com>
2014-06-02 17:07:05 +0300

    add slider

--- js/common.js
+++ js/common.js
@@ -7,4 +7,25 @@ head.ready(function() {
 		isResizable: false
 	});
 
+	$('.js-slider').each(function(){
+		var p = this.parentNode;
+		$(this).cycle({
+			fx: "scrollHorz",
+			autoHeight: "container",
+			timeout:  0,
+			carouselVisible: 3,
+			slideActiveClass: "is-active",
+			pagerActiveClass: "is-active",
+			disabledClass: "is-disabled",
+			slideClass: "testimonials__slide",
+			allowWrap: true,
+			carouselFluid: "true",
+			slides: $('.testimonials__slide', p),
+			prev:   $('.testimonials__prev', p),
+			next:   $('.testimonials__next', p),
+			pager: ".testimonials__pager",
+			pagerTemplate: "<span>•</span>"
+		});
+	});
+
 });
--- js/init.js
+++ js/init.js
@@ -1,5 +1,6 @@
 head.load(
 	"js/lib/jquery.js",
+	"js/lib/cycle.js",
 	"js/lib/masonry.min.js",
 	"js/common.js"
 );
